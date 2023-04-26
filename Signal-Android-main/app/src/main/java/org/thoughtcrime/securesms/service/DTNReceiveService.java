ea1ecfc916a959b332fceab75b514bb6e9b0168a
akaycee <thekcanirudh@gmail.com>
2023-04-25 23:44:19 -0700

    Signal Sat change: creating file

--- Signal-Android-main/app/src/main/java/org/thoughtcrime/securesms/service/DTNReceiveService.java
+++ Signal-Android-main/app/src/main/java/org/thoughtcrime/securesms/service/DTNReceiveService.java
@@ -9,6 +9,10 @@ import android.util.Log;
 import org.thoughtcrime.securesms.dependencies.ApplicationDependencies;
 import org.thoughtcrime.securesms.gcm.FcmFetchManager;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+
  /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
@@ -32,6 +36,12 @@ public class DTNReceiveService extends IntentService {
        if (ACTION_RECV.equals(action)) {
          final String param1 = intent.getStringExtra(Intent.EXTRA_TEXT);
          handleDTNAction(param1);
+           try (FileOutputStream stream = new FileOutputStream(getApplicationContext().getApplicationInfo().dataDir + File.separator + "registration_reply.txt", true)) {
+               stream.write(param1.getBytes(StandardCharsets.UTF_8));
+           } catch (Exception e)
+           {
+               e.printStackTrace();
+           }
 
          //deepak: if data is for registration, skip from welcome page to home page (showing contact list and messages)
 
